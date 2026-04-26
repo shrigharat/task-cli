e526bfd9870b4beb5bd00f86f186f8091be2c471
shrigharat <gharatshri014@gmail.com>
2026-04-26 17:25:21 +0530

    added handling for CLI args

--- cmd/main.go
+++ cmd/main.go
@@ -3,43 +3,127 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"task-cli/internal/task"
 	"time"
 )
 
+func exitWithError(message string) {
+	fmt.Println("Error: ", message)
+	os.Exit(1)
+}
+
+func addDefaultTasks(tasks []task.Task) []task.Task {
+	tasks = append(tasks, task.CreateTask(1, "Buy groceries", task.MediumPriority))
+	tasks = append(tasks, task.CreateTask(2, "Finish report", task.HighPriority))
+	tasks = append(tasks, task.CreateTask(3, "Call Alice", task.LowPriority))
+
+	return tasks
+}
+
+func validateRequiredArgument(argData string, argName string) string {
+	processedData := strings.TrimSpace(argData)
+	if processedData == "" {
+		exitWithError(fmt.Sprintf("%s cannot be empty", argName))
+	}
+
+	return processedData
+}
+
+func validateTaskId(taskIdString string, tasksLength int) int {
+	taskId, err := strconv.Atoi(taskIdString)
+	if err != nil {
+		exitWithError("Invalid task ID")
+	}
+	if taskId < 1 || taskId > tasksLength {
+		exitWithError("Invalid task ID")
+	}
+
+	return taskId
+}
+
 func main() {
 	args := os.Args[1:]
-	tasks := make([]task.Task, 0)
+	tasks := addDefaultTasks(make([]task.Task, 0))
 
-	fmt.Println("Args: ", args)
 	if len(args) == 0 {
 		fmt.Println("Error: Please provide an operation")
 		os.Exit(1)
 	}
 
 	operation := args[0]
-	taskTitle := args[1]
 
 	switch operation {
 	case "add":
-		newTask := task.Task{
-			Id:          uint8(len(tasks) + 1),
-			Title:       taskTitle,
-			Priority:    task.LowPriority,
-			Status:      task.StatusPending,
-			CreatedAt:   time.Now(),
-			UpdatedAt:   time.Now(),
-			DueDate:     time.Time{},
-			CompletedOn: time.Time{},
-		}
+		dataArgument := args[1]
+		taskTitle := validateRequiredArgument(dataArgument, "Title")
+		newTask := task.CreateTask(uint8(len(tasks)+1), taskTitle, task.LowPriority)
 		tasks = append(tasks, newTask)
 		fmt.Printf("Task \"%s\" added successfully\n", taskTitle)
-	case "list":
-		fmt.Println("Listing tasks")
-	case "complete":
-		fmt.Println("Completing task: ", taskTitle)
+	case "update":
+		dataArgument := args[1]
+		newTaskTitle := args[2]
+		taskId := validateTaskId(validateRequiredArgument(dataArgument, "Task ID"), len(tasks))
+		newTaskTitle = validateRequiredArgument(newTaskTitle, "New task title")
+		tasks[taskId-1].Title = newTaskTitle
+		fmt.Printf("Task \"%s\" updated successfully\n", newTaskTitle)
 	case "delete":
-		fmt.Println("Deleting task: ", taskTitle)
+		dataArgument := args[1]
+		taskId := validateTaskId(validateRequiredArgument(dataArgument, "Task ID"), len(tasks))
+		tasks = append(tasks[0:taskId-1], tasks[taskId:]...)
+		fmt.Printf("Task %d deleted successfully\n", taskId)
+	case "list":
+		filter := args[1]
+		filterStatus, err := task.ParseStatus(filter)
+		if err != nil {
+			exitWithError(fmt.Sprintf("Invalid status filter: %s", filter))
+		}
+		filteredTasks := make([]task.Task, 0)
+		switch filterStatus {
+		case task.StatusPending:
+			for _, currentTask := range tasks {
+				if currentTask.Status == task.StatusPending {
+					filteredTasks = append(filteredTasks, currentTask)
+				}
+			}
+		case task.StatusInProgress:
+			for _, currentTask := range tasks {
+				if currentTask.Status == task.StatusInProgress {
+					filteredTasks = append(filteredTasks, currentTask)
+				}
+			}
+		case task.StatusCompleted:
+			for _, currentTask := range tasks {
+				if currentTask.Status == task.StatusCompleted {
+					filteredTasks = append(filteredTasks, currentTask)
+				}
+			}
+		default:
+			filteredTasks = tasks
+		}
+
+		for index, currentTask := range tasks {
+			fmt.Printf("%d. %s\n", index+1, currentTask.Title)
+		}
+	case "complete":
+		dataArgument := args[1]
+		taskId := validateTaskId(validateRequiredArgument(dataArgument, "Task ID"), len(tasks))
+		tasks[taskId-1].Status = task.StatusCompleted
+		tasks[taskId-1].CompletedOn = time.Now()
+		fmt.Printf("Task %d completed successfully\n", taskId)
+	case "mark-in-progress":
+		dataArgument := args[1]
+		taskId := validateTaskId(validateRequiredArgument(dataArgument, "Task ID"), len(tasks))
+		tasks[taskId-1].Status = task.StatusInProgress
+		tasks[taskId-1].UpdatedAt = time.Now()
+		fmt.Printf("Task %d marked as in progress successfully\n", taskId)
+	case "mark-pending":
+		dataArgument := args[1]
+		taskId := validateTaskId(validateRequiredArgument(dataArgument, "Task ID"), len(tasks))
+		tasks[taskId-1].Status = task.StatusPending
+		tasks[taskId-1].UpdatedAt = time.Now()
+		fmt.Printf("Task %d marked as pending successfully\n", taskId)
 	default:
 		fmt.Println("Error: Invalid operation")
 		os.Exit(1)
--- internal/task/model.go
+++ internal/task/model.go
@@ -20,6 +20,31 @@ const (
 	StatusCompleted
 )
 
+var statusValueToLabelMap = map[TaskStatus]string{
+	StatusPending:    "pending",
+	StatusInProgress: "in-progress",
+	StatusCompleted:  "completed",
+}
+
+var statusLabelToValueMap = map[string]TaskStatus{
+	"pending":     StatusPending,
+	"in-progress": StatusInProgress,
+	"completed":   StatusCompleted,
+}
+
+func (s TaskStatus) String() string {
+	return statusValueToLabelMap[s]
+}
+
+func ParseStatus(label string) (TaskStatus, error) {
+	statusValue, ok := statusLabelToValueMap[label]
+	if !ok {
+		return 0, fmt.Errorf("invalid status label: %s", label)
+	}
+
+	return statusValue, nil
+}
+
 var priorityValueToLabelMap = map[TaskPriority]string{
 	LowPriority:    "low",
 	MediumPriority: "medium",
@@ -43,6 +68,19 @@ func ParsePriority(label string) (TaskPriority, error) {
 	}
 
 	return priorityValue, nil
+}
+
+func CreateTask(id uint8, title string, priority TaskPriority) Task {
+	return Task{
+		Id:          uint8(id),
+		Title:       title,
+		Priority:    priority,
+		Status:      StatusPending,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+		DueDate:     time.Time{},
+		CompletedOn: time.Time{},
+	}
 }
 
 type Task struct {
